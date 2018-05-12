dc8a63d3e9da2427b58e11e52a6eb42ebd3b980a
Azharuddin <azharuddin@neuronsolutions.com>
2018-05-12 17:33:38 +0530

    add storyboard

--- objectiveTravis/ViewController.m
+++ objectiveTravis/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+    @property (weak, nonatomic) IBOutlet UITextField *txfNumber1;
+    @property (weak, nonatomic) IBOutlet UITextField *txfNumer2;
+    
 @end
 
 @implementation ViewController
@@ -18,8 +20,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-
+- (IBAction)btn_click:(id)sender {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Do you want to say hello?"
+                                                    message:@"More info..."
+                                                   delegate:self
+                                          cancelButtonTitle:@"Cancel"
+                                          otherButtonTitles:@"Say Hello",nil];
+}
+    
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 0) {
+        NSLog(@"Cancel Tapped.");
+    }
+    else if (buttonIndex == 1) {
+        NSLog(@"OK Tapped. Hello World!");
+    }
+}
+    
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
